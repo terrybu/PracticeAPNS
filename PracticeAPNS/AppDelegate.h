562b04614104b3c8b65306d11f949d1cb0ea2796
Terry Bu <ktbucifer@gmail.com>
2014-12-10 17:23:46 -0500

    first commit

---
+++ PracticeAPNS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PracticeAPNS
+//
+//  Created by Aditya Narayan on 12/10/14.
+//  Copyright (c) 2014 TerryBuOrganization. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PracticeAPNS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PracticeAPNS
+//
+//  Created by Aditya Narayan on 12/10/14.
+//  Copyright (c) 2014 TerryBuOrganization. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
